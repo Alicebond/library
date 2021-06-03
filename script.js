32cfaab1c0ae4be5efe185b81d5584f7fea64665
Zhibin <zzbsdh000@outlook.com>
2021-06-03 16:54:00 -0230

    Add function to change read status

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 "use strict";
 
-const newBookBtn = document.querySelector("#newBook");
-const bgForm = document.querySelector("#bgForm");
-const closeFormBtn = document.querySelector("#closeForm");
 let myLibrary = [];
 
 function Book(title, author, pages, read) {
@@ -43,41 +40,20 @@ addBookToLibrary(crimeAndPunishment);
 addBookToLibrary(theLongGoodbye);
 addBookToLibrary(myLove);
 
-function displayBooks(myLibrary) {
-  myLibrary.map((book) => {
-    generateTable(book.info(), book.title);
-  });
-}
-
-function generateTable(text, title) {
-  const table = document.querySelector("table");
-  let row = table.insertRow();
-  row.id = `${title}`;
-  console.log(row);
-  let cell = row.insertCell();
-  cell.textContent = text;
-  createRrmoveBtn(row, title);
-}
-
-function createRrmoveBtn(row, title) {
-  let btn = document.createElement("button");
-  btn.type = "button";
-  btn.className = "remove-btn";
-  btn.textContent = "Remove";
-  btn.dataset.btnName = `${title}`;
-  row.appendChild(btn);
-
-  btn.addEventListener("click", (e) => {
-    let temp = e.target.dataset.btnName;
-    removeBook(temp);
-  });
-}
-
+const bgForm = document.querySelector("#bgForm");
+const newBookBtn = document.querySelector("#newBook");
 newBookBtn.addEventListener("click", () => {
   bgForm.style.display = "flex";
 });
 
+const closeFormBtn = document.querySelector("#closeForm");
 closeFormBtn.addEventListener("click", () => (bgForm.style.display = "none"));
+
+const submitBtn = document.querySelector("#submitForm");
+submitBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  getData();
+});
 
 function getData() {
   let title, author, pages, read;
@@ -96,15 +72,75 @@ function getData() {
   displayBooks(myLibrary);
 }
 
-const submitBtn = document.querySelector("#submitForm");
-submitBtn.addEventListener("click", (e) => {
-  e.preventDefault();
-  getData();
-});
+function displayBooks(myLibrary) {
+  myLibrary.map((book) => {
+    generateTable(book.info(), book.title);
+  });
+}
 
-function removeBook(temp) {
-  document.getElementById(temp).remove();
-  let x = myLibrary.findIndex((book) => book.title === temp);
+function generateTable(text, title) {
+  const table = document.querySelector("table");
+  let row = table.insertRow();
+  row.id = `${title}`;
+  let cell = row.insertCell();
+  cell.textContent = text;
+  createDropDownList(row, title);
+  createRrmoveBtn(row, title);
+}
+
+function createRrmoveBtn(row, title) {
+  let btn = document.createElement("button");
+  btn.type = "button";
+  btn.className = "remove-btn";
+  btn.textContent = "Remove";
+  btn.dataset.btnName = `${title}`;
+  row.appendChild(btn);
+
+  btn.addEventListener("click", (e) => {
+    let btnName = e.target.dataset.btnName;
+    removeBook(btnName);
+  });
+}
+
+function removeBook(btnName) {
+  document.getElementById(btnName).remove();
+  let x = myLibrary.findIndex((book) => book.title === btnName);
   myLibrary.splice(x, 1);
 }
+
+function createDropDownList(row, title) {
+  let options = ["Change Status", "Read", "Want to Read"];
+  let select = document.createElement("select");
+  select.name = "status";
+  select.dataset.title = `${title}`;
+
+  for (let i of options) {
+    let option = document.createElement("option");
+    option.value = i;
+    option.textContent = i;
+    select.appendChild(option);
+  }
+
+  let label = document.createElement("label");
+  label.htmlFor = "status";
+
+  row.appendChild(label).appendChild(select);
+
+  select.addEventListener("change", (e) => {
+    let changedStatus = e.target.value;
+    let changeTarget = e.target.dataset.title;
+    changeStatus(changedStatus, changeTarget);
+  });
+}
+
+function changeStatus(changedStatus, changeTarget) {
+  let x = myLibrary.findIndex((book) => book.title === changeTarget);
+  changedStatus === "Read"
+    ? (myLibrary[x].read = true)
+    : (myLibrary[x].read = false);
+
+  let el = document.getElementById(changeTarget);
+  el.firstChild.textContent = myLibrary[x].info();
+}
+
 displayBooks(myLibrary);
